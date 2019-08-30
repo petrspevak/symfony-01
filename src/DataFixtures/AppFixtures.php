93b40e50bf44dec836a8a0ce0e077a782412b95b
Petr Spevak <petr@spevak.eu>
2019-08-30 23:09:01 +0200

    uprava security.yaml + fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,23 +3,58 @@
 namespace App\DataFixtures;
 
 use App\Entity\MicroPost;
+use App\Entity\User;
 use DateTime;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use Exception;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
     /**
+     * @var UserPasswordEncoderInterface
+     */
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+    /**
      * @param ObjectManager $manager
      * @throws Exception
      */
-    public function load(ObjectManager $manager) : void
+    public function load(ObjectManager $manager): void
     {
-        for ($i=1; $i < 10; $i++) {
+        $this->loadUsers($manager);
+        $this->loadMicroPosts($manager);
+    }
+
+    private function loadUsers(ObjectManager $manager): void
+    {
+        $user = new User();
+        $user
+            ->setUsername('john')
+            ->setFullName('John Doe')
+            ->setEmail('john@doe.com')
+            ->setPassword($this->passwordEncoder->encodePassword($user, 'john123'));
+        $manager->persist($user);
+
+        $manager->flush();
+    }
+
+    /**
+     * @param ObjectManager $manager
+     * @throws Exception
+     */
+    private function loadMicroPosts(ObjectManager $manager): void
+    {
+        for ($i = 1; $i < 10; $i++) {
             $microPost = new MicroPost();
             $microPost
-                ->setText('Nahodny text' . random_int(1, 10000))
+                ->setText('Nahodny text ' . random_int(1, 10000))
                 ->setTime(new DateTime());
             $manager->persist($microPost);
         }
