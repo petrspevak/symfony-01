04c125cb13c9a89d207e30f93268373610770b08
Petr Spevak <petr@spevak.eu>
2019-09-01 00:32:13 +0200

    Voter

---
+++ src/Security/MicroPostVoter.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Security;
+
+use App\Entity\MicroPost;
+use App\Entity\User;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+
+class MicroPostVoter extends Voter
+{
+    public const EDIT = 'edit';
+    public const DELETE = 'delete';
+
+    /**
+     * Determines if the attribute and subject are supported by this voter.
+     *
+     * @param string $attribute An attribute
+     * @param mixed $subject The subject to secure, e.g. an object the user wants to access or any other PHP type
+     *
+     * @return bool True if the attribute and subject are supported, false otherwise
+     */
+    protected function supports($attribute, $subject): bool
+    {
+        if (!in_array($attribute, [self::EDIT, self::DELETE], true)) {
+            return false;
+        }
+
+        if (!($subject instanceof MicroPost)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Perform a single access check operation on a given attribute, subject and token.
+     * It is safe to assume that $attribute and $subject already passed the "supports()" method check.
+     *
+     * @param string $attribute
+     * @param mixed $subject
+     * @param TokenInterface $token
+     * 
+     * @return bool
+     */
+    protected function voteOnAttribute($attribute, $subject, TokenInterface $token): bool
+    {
+        $authenticatedUser = $token->getUser();
+
+        if (!$authenticatedUser instanceof User) {
+            return false;
+        }
+
+        /** @var MicroPost $microPost */
+        $microPost = $subject;
+
+        /** @noinspection TypeUnsafeComparisonInspection */
+        return $microPost->getUser() == $authenticatedUser;
+    }
+}
